4ba55b42a24d5247935f9319706b17df01b7f384
Gabriel <gabrielcarneirodesouza13@gmail.com>
2026-04-15 21:22:47 -0300

    adicionada config de segurança

---
+++ src/main/java/com/carneiro/agendador_de_tarefas/bussiness/dto/UsuarioDTO.java
@@ -0,0 +1,16 @@
+package com.carneiro.agendador_de_tarefas.bussiness.dto;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+    private String email;
+    private String senha;
+}
---
+++ src/main/java/com/carneiro/agendador_de_tarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.carneiro.agendador_de_tarefas.infrastructure.security;
+
+import com.carneiro.agendador_de_tarefas.bussiness.dto.UsuarioDTO;
+import com.carneiro.agendador_de_tarefas.infrastructure.client.UsuarioClient;
+import com.carneiro.usuario.infrastructure.entity.Usuario;
+import com.carneiro.usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl  {
+
+    @Autowired
+    private UsuarioClient client;
+
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+}
